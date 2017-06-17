dc8ae32765193f680b33b161ee555e3d6212d2d3
u958ganesan <u958ganesan@BUCHGanesW10.bsus.com>
2017-06-17 07:59:44 -0500

    Commit for successful bar-graph component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
 
 import { AppComponent } from './app.component';
+import { GroceriesData } from './classes/groceries-data';
+import { SalesDataService } from './shared/sales-data.service';
+import { GroceriesListComponent } from './components/groceries-list/groceries-list.component';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@NgModule( {
+    declarations: [
+        AppComponent,
+        GroceriesListComponent,
+        BarChartComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,ReactiveFormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(GroceriesData),
+        InMemoryWebApiModule.forRoot(SalesDataService)
+    ],
+    providers: [SalesDataService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
